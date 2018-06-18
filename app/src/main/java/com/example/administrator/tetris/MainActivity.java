62f5cebc978cb9fcd0871f69784111548a39a197
litong450937822 <li15929939221@163.com>
2018-06-18 16:11:59 +0800

    添加移动、旋转、边界判断，方块改为图片

--- app/src/main/java/com/example/administrator/tetris/MainActivity.java
+++ app/src/main/java/com/example/administrator/tetris/MainActivity.java
@@ -1,51 +1,75 @@
 package com.example.administrator.tetris;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Point;
+import android.graphics.Rect;
+import android.graphics.drawable.BitmapDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.FrameLayout;
 
 import com.example.administrator.tetris.view.TetrisView;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //    TetrisView tetrisView;
     //父容器宽、高
     public int xWidth, yHeight;
     //游戏区域控件
     View gameView;
-
     //辅助线画笔
     Paint linePaint;
     //方块画笔
     Paint blockPaint;
     //背景
-    boolean[][] background;
+    int[][] background;
     //方块
     Point[] blocks;
     int blockSize;
+    Bitmap block_brown,block_blue,block_green,block_pink,block_purple,block_red,block_yellow;
+    private Resources mResources;
+    private Paint mBitPaint;
+    private Rect mSrcRect, mDestRect;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
+        Button left = findViewById(R.id.left);
+        Button right = findViewById(R.id.right);
+        Button down = findViewById(R.id.down);
+        Button fallen = findViewById(R.id.fallen);
+        Button rotate = findViewById(R.id.rotate);
+        Button start = findViewById(R.id.start);
+        Button stop = findViewById(R.id.stop);
         //初始化数据
         initData();
         //初始化视图
         initView();
         //初始化背景
-        background = new boolean[10][20];
+        background = new int[10][20];
         //初始化方块
-        blocks = new Point[]{new Point(3, 0), new Point(3, 1), new Point(4, 1), new Point(5, 1)};
         blockSize = xWidth / background.length;
-
+        blocks = new Point[]{new Point(4, 1), new Point(3, 0), new Point(3, 1), new Point(5, 1)};
+        left.setOnClickListener(this);
+        right.setOnClickListener(this);
+        down.setOnClickListener(this);
+        fallen.setOnClickListener(this);
+        rotate.setOnClickListener(this);
+        start.setOnClickListener(this);
+        stop.setOnClickListener(this);
 
     }
 
@@ -56,6 +80,19 @@ public class MainActivity extends AppCompatActivity {
         xWidth = width * 2 / 3;
         //设置游戏区域的高度 = 宽度*2
         yHeight = xWidth * 2;
+        mResources = getResources();
+
+        block_brown = ((BitmapDrawable) mResources.getDrawable(R.drawable.brown_block)).getBitmap();
+        block_blue = ((BitmapDrawable) mResources.getDrawable(R.drawable.brown_block)).getBitmap();
+        block_green = ((BitmapDrawable) mResources.getDrawable(R.drawable.brown_block)).getBitmap();
+        block_pink = ((BitmapDrawable) mResources.getDrawable(R.drawable.brown_block)).getBitmap();
+        block_purple = ((BitmapDrawable) mResources.getDrawable(R.drawable.brown_block)).getBitmap();
+        block_red = ((BitmapDrawable) mResources.getDrawable(R.drawable.brown_block)).getBitmap();
+        block_yellow = ((BitmapDrawable) mResources.getDrawable(R.drawable.brown_block)).getBitmap();
+        int bitWidth = block_brown.getWidth();
+        int bitHeight = block_brown.getHeight();
+        mSrcRect = new Rect(0, 0, bitWidth, bitHeight);
+
 
     }
 
@@ -67,6 +104,10 @@ public class MainActivity extends AppCompatActivity {
         blockPaint = new Paint();
         blockPaint.setColor(0xff000000);
         blockPaint.setAntiAlias(true);
+
+        mBitPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mBitPaint.setFilterBitmap(true);
+        mBitPaint.setDither(true);
         //得到父容器
         FrameLayout layoutGame = findViewById(R.id.gameView);
         //实例化游戏区域
@@ -78,13 +119,17 @@ public class MainActivity extends AppCompatActivity {
                 super.onDraw(canvas);
                 //绘制方块
                 for (Point block : blocks) {
-                    canvas.drawRect(
-                            block.x * blockSize,
-                            block.y * blockSize,
+                    mDestRect = new Rect(block.x * blockSize, block.y * blockSize,
                             block.x * blockSize + blockSize,
-                            block.y * blockSize + blockSize,
-                            blockPaint
-                    );
+                            block.y * blockSize + blockSize);
+                    canvas.drawBitmap(block_brown,mSrcRect,mDestRect,mBitPaint);
+//                    canvas.drawRect(
+//                            block.x * blockSize,
+//                            block.y * blockSize,
+//                            block.x * blockSize + blockSize,
+//                            block.y * blockSize + blockSize,
+//                            blockPaint
+//                    );
                 }
                 //绘制辅助线
                 for (int x = 0; x < background.length; x++) {
@@ -110,5 +155,71 @@ public class MainActivity extends AppCompatActivity {
         assert wm != null;
         wm.getDefaultDisplay().getMetrics(outMetrics);
         return outMetrics.widthPixels;
+    }
+
+    @SuppressLint("WrongConstant")
+    @Override
+    public void onClick(View view) {
+        int vid = view.getId();
+        switch (vid) {
+            case R.id.left:
+                move(-1, 0);
+                break;
+            case R.id.right:
+                move(1, 0);
+                break;
+            case R.id.down:
+                move(0, 1);
+                break;
+            case R.id.fallen:
+                break;
+            case R.id.rotate:
+                rotate();
+                break;
+            case R.id.stop:
+                break;
+            case R.id.start:
+                break;
+        }
+        //重绘view
+        gameView.invalidate();
+    }
+
+    //移动
+    public boolean move(int x, int y) {
+        //遍历当前方块儿数组 每一块儿加上偏移量
+        for (Point block : blocks) {
+            if (checkBoundary(block.x + x, block.y + y))
+                return false;
+        }
+        for (Point block : blocks) {
+            block.x += x;
+            block.y += y;
+        }
+        return true;
+    }
+
+    //旋转
+    public boolean rotate() {
+        for (Point block : blocks) {
+            //笛卡尔公式 顺时针旋转90°
+            int checkX = -block.y + blocks[0].y + blocks[0].x;
+            int checkY = block.x - blocks[0].x + blocks[0].y;
+            if (checkBoundary(checkX, checkY)) {
+                return false;
+            }
+        }
+        for (Point block : blocks) {
+            int checkX = -block.y + blocks[0].y + blocks[0].x;
+            int checkY = block.x - blocks[0].x + blocks[0].y;
+            block.x = checkX;
+            block.y = checkY;
+        }
+        return true;
+    }
+
+    //边界判断
+    public boolean checkBoundary(int x, int y) {
+        return (x < 0 || y < 0 || x >= background.length || y >= background[0].length);
     }
 }
--- app/src/main/java/com/example/administrator/tetris/view/TetrisView.java
+++ app/src/main/java/com/example/administrator/tetris/view/TetrisView.java
@@ -1,21 +1,18 @@
 package com.example.administrator.tetris.view;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Point;
-import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
-import android.widget.FrameLayout;
 
 public class TetrisView extends View {
     //父容器宽、高
     public int xWidth, yHeight;
     //屏幕宽度
     private int width;
-    //游戏区域控件
-    View gameView;
 
     //辅助线画笔
     Paint linePaint;
@@ -27,22 +24,39 @@ public class TetrisView extends View {
     Point[] blocks;
     int blockSize;
 
-    public TetrisView(Context context, @Nullable AttributeSet attrs) {
+    public TetrisView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        //初始化数据
-        initData();
         //初始化视图
         initView();
-        //初始化背景
-        background = new boolean[10][20];
-        //初始化方块
-        blocks = new Point[]{new Point(3, 0),new Point(3, 1),new Point(4, 1),new Point(5, 1)};
-        blockSize = xWidth / background.length;
+
     }
 
     @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+    }
+
+    @SuppressLint("DrawAllocation")
+    @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        width = canvas.getWidth();
+
+        //初始化数据
+        initData();
+
+        //初始化背景
+        background = new boolean[10][20];
+        //初始化方块
+        blocks = new Point[]{new Point(3, 0), new Point(3, 1), new Point(4, 1), new Point(5, 1)};
+        blockSize = xWidth / background.length;
+        //绘制辅助线
+        for (int x = 0; x < background.length; x++) {
+            canvas.drawLine(x * blockSize, 0, x * blockSize, canvas.getHeight(), linePaint);
+        }
+        for (int y = 0; y < background[0].length; y++) {
+            canvas.drawLine(0, y * blockSize, width, y * blockSize, linePaint);
+        }
         for (Point block : blocks) {
             canvas.drawRect(
                     block.x * blockSize,
@@ -51,13 +65,6 @@ public class TetrisView extends View {
                     block.y * blockSize + blockSize,
                     blockPaint
             );
-        }
-        //绘制辅助线
-        for (int x = 0; x < background.length; x++) {
-            canvas.drawLine(x * blockSize, 0, x * blockSize, gameView.getHeight(), linePaint);
-        }
-        for (int y = 0; y < background[0].length; y++) {
-            canvas.drawLine(0, y * blockSize, gameView.getWidth(), y * blockSize, linePaint);
         }
     }
 
@@ -69,9 +76,6 @@ public class TetrisView extends View {
 
     }
 
-    public void setWidth(int width) {
-        this.width = width;
-    }
 
     public void initView() {
         linePaint = new Paint();
@@ -81,10 +85,10 @@ public class TetrisView extends View {
         blockPaint = new Paint();
         blockPaint.setColor(0xff000000);
         blockPaint.setAntiAlias(true);
-        //设置游戏区域大小
-        gameView.setLayoutParams(new FrameLayout.LayoutParams(xWidth, yHeight));
-        gameView.setBackgroundColor(0x10000000);
     }
 
-
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+    }
 }
