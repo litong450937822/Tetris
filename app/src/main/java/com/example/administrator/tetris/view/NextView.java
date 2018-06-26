dcada209f3c96335e93da7f78370f2e4c64cb0ae
litong450937822 <li15929939221@163.com>
2018-06-26 22:01:01 +0800

    游戏开始倒计时添加NextView遮罩

--- app/src/main/java/com/example/administrator/tetris/view/NextView.java
+++ app/src/main/java/com/example/administrator/tetris/view/NextView.java
@@ -2,18 +2,25 @@ package com.example.administrator.tetris.view;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
+
+import com.example.administrator.tetris.Config;
 import com.example.administrator.tetris.control.GameControl;
 
 public class NextView extends View {
     private GameControl gameControl;
+    private Paint startPaint;
 
 
     public NextView(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         this.setBackgroundColor(0xffeeeeee);
+        startPaint = new Paint();
+        startPaint.setColor(0xFFCCCCCC);
+        startPaint.setAntiAlias(true);
     }
 
     public void setGameControl(GameControl gameControl) {
@@ -24,5 +31,8 @@ public class NextView extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         gameControl.drawNext(canvas);
+        if (gameControl.time < 0) {
+            canvas.drawRect(0,0, this.getWidth() , this.getHeight(), startPaint);
+        }
     }
 }
